d9bbc8eb31b35f07d13b2e6cffa8d030d0131a64
unknown <adonix1014@gmail.com>
2026-01-17 13:31:15 +0800

    Fix: Add ping and diag probes

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,7 @@
-export default function handler(req, res) {
-    res.status(200).send("API is working!");
-}
+module.exports = (req, res) => {
+    res.status(200).json({
+        message: 'Pong!',
+        env_token_exists: !!process.env.LINE_CHANNEL_ACCESS_TOKEN,
+        time: new Date().toISOString()
+    });
+};
