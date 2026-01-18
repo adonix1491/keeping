dd21f29e696472156f7eeae3db3ff3133809ada0
unknown <adonix1014@gmail.com>
2026-01-18 12:07:07 +0800

    Remove unused APIs to stay under Vercel function limit

--- api/test.js
+++
@@ -1,6 +0,0 @@
-export default function handler(request, response) {
-    response.status(200).json({
-        message: 'Hello from Vercel Node.js!',
-        time: new Date().toISOString(),
-    });
-}
